f6d9bcd4e3724d377c3708813acc3db04892ee80
Ayaan Abdirashid <ayaanabdirashid>
2021-02-23 16:41:11 +0100

    message

---
+++ src/script.js
@@ -0,0 +1,17 @@
+import '../styles.scss';
+
+// in JS only, files in "./static" are loaded without "static" prefix
+const root = document.getElementById('root');
+root.innerHTML += '<img src="images/kitten.jpg" class="w-1/4 m-auto" />';
+
+// const btn = documentById('btn');
+// const modal = documentById('idModal');
+// const span = documentByClassName('fermer');
+
+// btn.onclick = function () {
+//   modal.style.display = 'block';
+// };
+
+// span.onclick = function () {
+//   modal.style.display = 'none';
+// };
